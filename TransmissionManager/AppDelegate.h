447854a77f441135e6bee6a62815ba1c32100cc7
葛岳 <GeYue@g-mbp-dev.local>
2016-06-28 20:18:39 +0800

    Initial Commit

---
+++ TransmissionManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransmissionManager
+//
+//  Created by 葛岳 on 16/6/28.
+//  Copyright © 2016年 葛岳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransmissionManagerTests/TransmissionManagerTests.m
@@ -0,0 +1,39 @@
+//
+//  TransmissionManagerTests.m
+//  TransmissionManagerTests
+//
+//  Created by 葛岳 on 16/6/28.
+//  Copyright © 2016年 葛岳. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TransmissionManagerTests : XCTestCase
+
+@end
+
+@implementation TransmissionManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
