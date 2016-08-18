16b1bf88988976e90a19da1aa24c435aa0471c42
GeYue <GeYue.Me@Gmail.com>
2016-08-18 19:27:36 +0800

    Add "Background Location setting" in project configuration. Try location function, not work on simulator?

--- TransmissionManager/UIInfoViewController.m
+++ TransmissionManager/UIInfoViewController.m
@@ -10,6 +10,7 @@
 #import "ExtSCGifImageView.h"
 
 #import <AMapFoundationKit/AMapFoundationKit.h>
+#import <AMapLocationKit/AMapLocationKit.h>
 #import <AMapSearchKit/AMapSearchKit.h>
 #import <MAMapKit/MAMapKit.h>
 
@@ -18,6 +19,8 @@
     
     MAMapView * _mapView;
 }
+@property (strong, nonatomic) IBOutlet UIButton *LocationButton;
+@property (nonatomic, strong) AMapLocationManager * locationManager;
 @end
 
 @implementation UIInfoViewController
@@ -32,11 +35,38 @@
     
     [AMapServices sharedServices].apiKey = @"951fc1b331728f94dbd8ac710091430c";
     
-    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(30, 300, CGRectGetWidth(self.view.bounds)-60, CGRectGetHeight(self.view.bounds)-100)];
+    _mapView = [[MAMapView alloc] initWithFrame:CGRectMake(30, 300, CGRectGetWidth(self.view.bounds)-60, CGRectGetHeight(self.view.bounds)-350)];
     _mapView.delegate = self;
+    
+    self.locationManager = [[AMapLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager setPausesLocationUpdatesAutomatically:NO];
+    [self.locationManager setAllowsBackgroundLocationUpdates:YES];
     
     [self.view addSubview:gifImageView];
     [self.view addSubview:_mapView];
+}
+
+- (void) startSerialLocation {
+    [self.locationManager startUpdatingLocation];
+}
+
+- (void) stopSerialLocation {
+    [self.locationManager stopUpdatingLocation];
+}
+
+- (IBAction)ClickedLocation:(id)sender {
+    [self startSerialLocation];
+}
+
+#pragma mark - AMapLocationManagerDelegate
+
+- (void) amapLocationManager:(AMapLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"%s, amapLocationManager = %@, error = %@", __func__, [manager class], error);
+}
+
+- (void) amapLocationManager:(AMapLocationManager *)manager didUpdateLocation:(CLLocation *)location {
+    NSLog(@"location:{lat:%f, lon:%f, accuracy:%f}", location.coordinate.latitude, location.coordinate.longitude, location.horizontalAccuracy);
 }
 
 @end
